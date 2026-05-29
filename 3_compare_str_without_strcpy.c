0d563bb96f6d469c5cd15001bfb2dcf26a2250d2
Luis Carrion <luiscarrionrdz@gmail.com>
2026-05-29 16:50:48 -0700

    Fixed a bug on exercise 3

--- 3_compare_str_without_strcpy.c
+++ 3_compare_str_without_strcpy.c
@@ -1,36 +1,35 @@
 #include <stdio.h>
 
 int main(void) {
-    char arr_1[100];
-    char arr_2[100];
+    char str_1[100];
+    char str_2[100];
 
     int is_same = 1; // We assume both are equal at first
 
     printf("Introduce the first text:");
-    fgets(arr_1, sizeof(arr_1), stdin);
+    fgets(str_1, sizeof(str_1), stdin);
 
     printf("Introduce the second text:");
-    fgets(arr_2, sizeof(arr_2), stdin);
+    fgets(str_2, sizeof(str_2), stdin);
 
     // Delete the newline termination
     for (int j = 0; j < 2; j++) {
-        char* c = (j==0) ? arr_1 : arr_2;
-        int index = 0;
+        char* current_str = (j==0) ? str_1 : str_2;
+        int char_index = 0;
 
-        while (c[index] != '\0') {
-            if (c[index] == '\n') {
-                c[index] = '\0';
+        while (current_str[char_index] != '\0') {
+            if (current_str[char_index] == '\n') {
+                current_str[char_index] = '\0';
                 break;
             }
+            char_index++;
         }
-
-        index++;
     }
 
     // We compare strings
     int i = 0;
-    while (arr_1[i] != '\0' || arr_2[i] != '\0') {
-        if (arr_1[i] != arr_2[i]){
+    while (str_1[i] != '\0' || str_2[i] != '\0') {
+        if (str_1[i] != str_2[i]){
             is_same = 0;
             break;
         }
